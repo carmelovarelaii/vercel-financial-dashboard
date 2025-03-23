8f0feedee37c180f849009e6bbe00aa7cf49fde8
carmelovarelaii <carmelovarelaii@gmail.com>
2025-03-23 18:44:11 +0000

    Added alt for mobile image

--- app/page.tsx
+++ app/page.tsx
@@ -36,6 +36,7 @@ export default function Page() {
             width={560}
             height={620}
             className="block md:hidden"
+            alt="Screenshots of the dashboard project showing mobile version"
           />
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
